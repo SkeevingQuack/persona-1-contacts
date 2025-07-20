7aa70211a5f77e71f49cbbde1966f03518a423ac
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2025-07-20 05:19:48 -0500

    fix listerner

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ function replaceMember() {
 	if (enabledMC === 0) {
 		enabledMC = 1;
 		member1.innerHTML = "<th>MC</th><td>Persuade</td><td>Taunt</td><td>Invite</td><td>Sing</td>"
+	} else if (enabledMC === 1) {
+		enabledMC = 0;
+		member1.innerHTML = "<th></th><td></td><td></td><td></td><td></td>";
 	}
 }
 
-buttonMC.addEventListerner("click", replaceMember);	
+buttonMC.addEventListener("click", replaceMember);	
