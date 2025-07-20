b5207554b86aa52a475e6535ef631e41da2a9d80
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2025-07-20 05:09:20 -0500

    major initial test

---
+++ main.js
@@ -0,0 +1,21 @@
+const wise = document.querySelector(".wise");
+const foolish = document.querySelector(".foolish");
+const joyful = document.querySelector(".joyful");
+const gloomy = document.querySelector(".gloomy");
+const timid = document.querySelector(".timid");
+const forceful = document.querySelector(".forceful");
+const snappish = document.querySelector(".snappish");
+const haughty = document.querySelector(".haughty");
+
+const buttonMC = document.querySelector(".buttonMC");
+let enabledMC = 0;
+const member1 = document.querySelector(".member1");
+
+function replaceMember() {
+	if (enabledMC === 0) {
+		enabledMC = 1;
+		member1.innerHTML = "<th>MC</th><td>Persuade</td><td>Taunt</td><td>Invite</td><td>Sing</td>"
+	}
+}
+
+buttonMC.addEventListerner("click", replaceMember);	
